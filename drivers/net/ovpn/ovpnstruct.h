ed6ec7559358b39ce65c4eb50a48f0651ccffd24
Antonio Quartulli <antonio@openvpn.net>
2024-03-01 15:08:40 +0100

    ovpn: add basic interface creation/destruction/management routines
    Add basic infrastructure for handling ovpn interfaces.
    
    Signed-off-by: Antonio Quartulli <antonio@openvpn.net>

--- drivers/net/ovpn/ovpnstruct.h
+++ drivers/net/ovpn/ovpnstruct.h
@@ -11,15 +11,21 @@
 #define _NET_OVPN_OVPNSTRUCT_H_
 
 #include <net/net_trackers.h>
+#include <uapi/linux/if_link.h>
+#include <uapi/linux/ovpn.h>
 
 /**
  * struct ovpn_priv - per ovpn interface state
  * @dev: the actual netdev representing the tunnel
  * @dev_tracker: reference tracker for associated dev
+ * @registered: whether dev is still registered with netdev or not
+ * @mode: device operation mode (i.e. p2p, mp, ..)
  */
 struct ovpn_priv {
 	struct net_device *dev;
 	netdevice_tracker dev_tracker;
+	bool registered;
+	enum ovpn_mode mode;
 };
 
 #endif /* _NET_OVPN_OVPNSTRUCT_H_ */
