dfb0b441f286033aacac9ef2f17ee6a2b0baf923
Antonio Quartulli <antonio@openvpn.net>
2024-03-01 11:53:00 +0100

    ovpn: add basic netlink support
    This commit introduces basic netlink support with family
    registration/unregistration functionalities and stub pre/post-doit.
    
    More importantly it introduces the YAML uAPI description along
    with its auto-generated files:
    - include/uapi/linux/ovpn.h
    - drivers/net/ovpn/netlink-gen.c
    - drivers/net/ovpn/netlink-gen.h
    
    Cc: donald.hunter@gmail.com
    Signed-off-by: Antonio Quartulli <antonio@openvpn.net>

---
+++ drivers/net/ovpn/ovpnstruct.h
@@ -0,0 +1,25 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*  OpenVPN data channel offload
+ *
+ *  Copyright (C) 2019-2024 OpenVPN, Inc.
+ *
+ *  Author:	James Yonan <james@openvpn.net>
+ *		Antonio Quartulli <antonio@openvpn.net>
+ */
+
+#ifndef _NET_OVPN_OVPNSTRUCT_H_
+#define _NET_OVPN_OVPNSTRUCT_H_
+
+#include <net/net_trackers.h>
+
+/**
+ * struct ovpn_priv - per ovpn interface state
+ * @dev: the actual netdev representing the tunnel
+ * @dev_tracker: reference tracker for associated dev
+ */
+struct ovpn_priv {
+	struct net_device *dev;
+	netdevice_tracker dev_tracker;
+};
+
+#endif /* _NET_OVPN_OVPNSTRUCT_H_ */
