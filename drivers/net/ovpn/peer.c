0ba28840715620a9ce09877a92d8cacc487cc1ae
Antonio Quartulli <antonio@openvpn.net>
2024-03-03 01:30:27 +0100

    ovpn: introduce the ovpn_socket object
    This specific structure is used in the ovpn kernel module
    to wrap and carry around a standard kernel socket.
    
    ovpn takes ownership of passed sockets and therefore an ovpn
    specific objects is attached to them for status tracking
    purposes.
    
    Initially only UDP support is introduced. TCP will come in a later
    patch.
    
    Cc: willemdebruijn.kernel@gmail.com
    Signed-off-by: Antonio Quartulli <antonio@openvpn.net>

--- drivers/net/ovpn/peer.c
+++ drivers/net/ovpn/peer.c
@@ -258,6 +258,12 @@ static void ovpn_peer_remove_work(struct work_struct *work)
 	struct ovpn_peer *peer = container_of(work, struct ovpn_peer,
 					      remove_work);
 
+	/* the peer has been unhashed and is being deactivated - we can
+	 * now switch off the socket and drop its reference
+	 */
+	if (peer->sock)
+		ovpn_socket_release(peer->sock);
+
 	/* reference from ovpn->peer or hashtable dropped */
 	ovpn_peer_put(peer);
 }
